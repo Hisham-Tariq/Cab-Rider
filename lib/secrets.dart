6ae3cd15c8d480d49ac722846ff0af2d851c8902
Hisham Tariq <63235813+Hisham-Tariq@users.noreply.github.com>
2022-09-10 07:00:17 -0700

    Update secrets.dart

--- lib/secrets.dart
+++ lib/secrets.dart
@@ -1,2 +1,2 @@
-String GOOGLE_API_KEY = 'AIzaSyCGZq6dqMEbYefSbw8iJ-g5zUtzolLDKbA';
-// https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=32.6052041,74.0796153&radius=1500&keyword=Ajwa&key=AIzaSyCGZq6dqMEbYefSbw8iJ-g5zUtzolLDKbA
+String GOOGLE_API_KEY = '';
+// https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=32.6052041,74.0796153&radius=1500&keyword=Ajwa&key=key
